144fad81400ee8e9ebd65a322b2683fd0274faa7
Benjamin Alcala <benalcalajr@gmail.com>
2020-01-30 23:06:36 -0800

    changes to socket io port

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -40,7 +40,7 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    const socket = openSocket('http://localhost:8080');
+    const socket = openSocket(`'https://blogapp-ben.herokuapp.com:${process.env.PORT}`);
     socket.on('posts', data => {
       if (data.action === 'create') {
         this.addPost(data.post);
